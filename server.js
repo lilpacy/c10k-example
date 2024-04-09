44298dd5bf0107eb51587bcefdd5dd96d7dd07bb
lilpacy <revivedtomorrow@gmail.com>
2024-04-09 14:43:27 +0900

    jsもsocketの低レイヤーモジュール使うように変更

--- server.js
+++ server.js
@@ -1,11 +1,14 @@
-const http = require("http");
+const net = require("net");
 
-const server = http.createServer((req, res) => {
-  res.writeHead(200, { "Content-Type": "text/plain" });
-  res.end("Hello, World!");
-});
+const handleConnection = socket => {
+  socket.on("data", data => {
+    socket.write("HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\n\r\nHello, World!");
+    socket.end();
+  });
+};
+
+const server = net.createServer(handleConnection);
 server.maxConnections = 1000;
-
 server.listen(8000, () => {
   console.log("Server running at http://localhost:8000/");
 });
